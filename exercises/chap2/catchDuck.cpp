33db08723a82a098029a7c04f17482368816efbf
marktwainme <a1264879714@126.com>
2022-11-23 15:25:21 +0800

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	modified:   exercises/chap2/catchDuck.cpp

---
+++ exercises/chap2/catchDuck.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "---4 个人，抓鸭子游戏开始了---" << endl;
+    cout << "A 说：抓鸭子" << endl;
+    cout << "B 说：抓几只" << endl;
+    cout << "C 说：抓2只" << endl;
+    cout << "D 说：抓住了" << endl;
+    cout << "A 说：嘎" << endl;
+    cout << "B 说：嘎" << endl;
+    cout << "C 说：抓鸭子" << endl;
+    cout << "D 说：抓几只" << endl;
+    cout << "A 说：抓一百只" << endl;
+    cout << "B 说：嘎" << endl;
+    cout << "C 说：嘎" << endl;
+    cout << "GAGAGA......" << endl;
+    return 0;
+}
